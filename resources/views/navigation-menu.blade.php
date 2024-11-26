b9eb073f39a3f4c9f175da6003ce95fb28b56515
nicole <nicole@gmail.com>
2024-11-26 13:54:51 -0500

    CHAT & DASHBOARD + PULL erick y agregado nav

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('verificacion') }}" :active="request()->routeIs('verificacion')">
+                        {{ __('Mis Denuncias') }}
+                    </x-nav-link>
                 </div>
             </div>
 
